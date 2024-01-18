e3e88c0271e578836ebcc1b7d5a2fc37a4fdec09
Kobrar0112 <85014618+Kobrar0112@users.noreply.github.com>
2024-01-18 10:33:08 +0300

    Update Практическая N1.cpp

---
+++ Практическая N1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"\nНет радости... той, что рисует";
+    cout<<"\n\tНа сердце улыбку весны";
+    cout<<"\n\t\tИ сердце по воле тоскует,";
+    cout<<"\n\t\t\tСадясь на скамью запасных.";
+    
+
+
+
+
+
+
+    return 0;
+}
